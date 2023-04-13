a280288706773cc29271f7c715960289bb5994d1
dhchoi-hs <donkman.hs@gmail.com>
2023-04-13 17:49:13 +0900

    feat: add parameter setting cuda index for torch device.

--- model/utils/get_torch_device.py
+++ model/utils/get_torch_device.py
@@ -1,11 +1,12 @@
 import torch
 
 
-def get_torch_device():
+def get_torch_device(cuda_idx=None):
     if torch.backends.mps.is_available():
         device = torch.device('mps')
     elif torch.cuda.is_available():
-        device = torch.device('cuda')
+        cuda = f'cuda:{cuda_idx}' if cuda_idx is not None else 'cuda'
+        device = torch.device(cuda)
     else:
         device = torch.device('cpu')
     
